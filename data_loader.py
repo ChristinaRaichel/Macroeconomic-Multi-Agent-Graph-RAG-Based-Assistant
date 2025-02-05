8b50a38d3607c663a6f6cad855011a4bacde6f77
Christina_Raichel_Francis <celestianc.73@gmail.com>
2025-02-05 16:37:19 +0000

    gdp code added

--- data_loader.py
+++ data_loader.py
@@ -1,1 +1,27 @@
 
+import requests
+import pandas as pd
+import logging
+
+gdp_url = "http://api.worldbank.org/v2/country/all/indicator/NY.GDP.MKTP.CD?format=json"
+
+gdp_response = requests.get(gdp_url)
+gdp_data = gdp_response.json()
+
+countries = []
+gdp_values = []
+years = []
+
+for entry in gdp_data[1]:  # API response structure
+    country = entry["country"]["value"]
+    year = entry["date"]
+    gdp = entry["value"]
+    
+    countries.append(country)
+    years.append(year)
+    gdp_values.append(gdp)
+
+gdp_df = pd.DataFrame({"Country": countries, "Year": years, "GDP": gdp_values})
+print(gdp_df.head())
+
+
